be8c95f717dea64659386d6cf50ae2308b19d24c
Philos Kim <philos99@gmail.com>
2021-05-11 15:04:22 +0900

    book 4.5.5

--- app/src/main/app/ui.cljs
+++ app/src/main/app/ui.cljs
@@ -4,12 +4,27 @@
     [com.fulcrologic.fulcro.dom :as dom]))
 
 (defsc Person [this {:person/keys [name age]}]
-  (dom/div
-    (dom/p "Name: " name)
-    (dom/p "Age: " age)))
+  (dom/li
+    (dom/h5 (str name " (age: " age ")"))))
 
-(def ui-person (comp/factory Person))
+;; The keyfn generates a react key for each element based on props. See React documentation on keys.
+(def ui-person (comp/factory Person {:keyfn :person/name}))
 
-(defsc Root [this props]
+(defsc PersonList [this {:list/keys [label people]}]
   (dom/div
-    (ui-person {:person/name "Joe" :person/age 22})))
+    (dom/h4 label)
+    (dom/ul
+      (map ui-person people))))
+
+(def ui-person-list (comp/factory PersonList))
+
+(defsc Root [this {:keys [ui/react-key]}]
+  (let [ui-data {:friends {:list/label "Friends" :list/people
+                                              [{:person/name "Sally" :person/age 32}
+                                               {:person/name "Joe" :person/age 22}]}
+                 :enemies {:list/label "Enemies" :list/people
+                                              [{:person/name "Fred" :person/age 11}
+                                               {:person/name "Bobby" :person/age 55}]}}]
+    (dom/div
+      (ui-person-list (:friends ui-data))
+      (ui-person-list (:enemies ui-data)))))
